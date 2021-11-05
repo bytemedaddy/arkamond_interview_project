dc1fcd664073747b9d6624e2e1dabedf2974b294
nikan zamani <zamaninikan@gmail.com>
2021-11-06 00:42:50 +0330

    fresh start

--- README.md
+++
@@ -1,2 +0,0 @@
-# arkamond_interview_project
-google authentication and drive access with django
--- google_oauth_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GoogleOauthAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'google_oauth_app'
